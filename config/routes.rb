538659b71d27875da2d06f4d9707dc9fe89c5d65
dionomusuko <ruby.school@gmail.com>
2019-12-21 20:03:45 +0900

    fix: routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'rooms/new'
   get 'rooms/edit'
   root 'home#index'
-  devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    post 'login', to: 'devise/sessions#create'
+    delete 'signout', to: 'devise/sessions#destroy'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
