e4ae6b51131d84fe76169b388446435872998b73
dionomusuko <ruby.school@gmail.com>
2020-01-06 16:30:58 +0900

    fix: room, user model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ApplicationRecord
-  has_many :users, through: :group
+  has_many :users, through: :groups
   has_many :groups
-  accepts_nested_attributes_for :group
+  accepts_nested_attributes_for :groups
 end
