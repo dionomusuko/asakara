bcaf5900827c63e8e166c5e67c7e812b4ec4e0d1
dionomusuko <ruby.school@gmail.com>
2020-01-15 16:57:45 +0900

    Add: groups_controller

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,23 @@
+class GroupsController < ApplicationController
+  def create
+    user = current_user
+    room = Room.find(params[:room_id])
+    if Group.create(user_id: user.id, room_id: room.id)
+      redirect_to rooms_path, notice: "#{room.title}にjoinしました"
+    else
+      redirect_to rooms_path
+    end
+
+  end
+
+  def destroy
+    user = current_user
+    room = Room.find(params[:room_id])
+    if group == Group.find_by(user_id: user.id, room_id: room.id)
+      group.delete
+      redirect_to rooms_path
+    else
+      redirect_to rooms_path
+    end
+  end
+end
