0d87ff345901eedf0aec2248750730dba49d1f0d
dionomusuko <ruby.school@gmail.com>
2020-01-15 14:25:24 +0900

    Fix: application

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
+    get "sign_up", :to => "users/registrations#new"
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy"
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
