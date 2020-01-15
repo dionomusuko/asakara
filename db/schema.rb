3e8964896fc8db067b45f9ab9cac63f60097a875
dionomusuko <ruby.school@gmail.com>
2020-01-15 17:47:36 +0900

    Fix: room-model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_21_152951) do
+ActiveRecord::Schema.define(version: 2020_01_15_083749) do
 
   create_table "groups", force: :cascade do |t|
     t.integer "user_id"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_12_21_152951) do
 
   create_table "rooms", force: :cascade do |t|
     t.string "title"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
