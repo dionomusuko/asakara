d3971ad6c4144189b1f4c7ccf82820ad63ad721c
dionomusuko <ruby.school114@gmail.com>
2020-01-22 18:00:09 +0900

    Comp: chat

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,12 +1,15 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "chat_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    chat = Chat.create!(content: data['chat'])
+    template = ApplicationController.renderer.render(partial: 'chats/chat', locals: {chat: chat})
+    ActionCable.server.broadcast 'chat_channel', template
   end
 end
