eb2c41f92d3a400d2328ec7fe9c5d1c07baf973a
dionomusuko <ruby.school114@gmail.com>
2020-01-29 21:39:31 +0900

    Fix: migration

--- db/migrate/20191221094720_create_rooms.rb
+++ db/migrate/20191221094720_create_rooms.rb
@@ -2,8 +2,7 @@ class CreateRooms < ActiveRecord::Migration[5.2]
   def change
     create_table :rooms do |t|
       t.string :title
-      t.string :string
-
+      t.string :detail
       t.timestamps
     end
   end
